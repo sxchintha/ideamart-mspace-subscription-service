0882544a316b262904d53576ee7fba64bc80985f
Sachintha Gunaratne <sachinthasl99@gmail.com>
2024-07-16 09:08:43 +0530

    refactor: mobitelController.js and utils files

--- controllers/mobitelController.js
+++ controllers/mobitelController.js
@@ -1,44 +1,23 @@
 import asyncHandler from "express-async-handler";
-import { formatSubscriberId } from "../utils/formatSubscriberId.js";
-import { checkMobitelStatusCode } from "../utils/checkStatusCode.js";
+import validateSubscriberId from "../utils/validateSubscriberId.js";
+import handleApiResponse from "../utils/handleApiResponse.js";
 import {
   getStatus,
   subscribeToService,
   unsubscribeFromService,
 } from "../services/mobitelServices.js";
 
-// Utility function to validate subscriberId
-const validateSubscriberId = (subscriberId, res) => {
-  if (!subscriberId) {
-    res.status(400).send({ error: "subscriberId is required" });
-    throw new Error("subscriberId is required");
-  }
-};
-
-// Utility function to handle API response
-const handleApiResponse = (response, res) => {
-  if (checkMobitelStatusCode(response?.data?.statusCode)) {
-    res.status(200).send({
-      apiStatus: "success",
-      ...response.data,
-    });
-  } else {
-    res.status(response.status === 200 ? 400 : response.status).send({
-      apiStatus: "error",
-      ...response.data,
-    });
-  }
-};
-
 const handleSubscribe = asyncHandler(async (req, res) => {
   const { subscriberId } = req.body;
-  validateSubscriberId(subscriberId, res);
-  const formattedSubscriberId = formatSubscriberId(subscriberId);
+
+  const formattedSubscriberId = validateSubscriberId(subscriberId, res);
 
   const status = await getStatus(formattedSubscriberId);
 
   if (status?.data?.subscriptionStatus === "REGISTERED") {
-    res.status(200).send({ apiStatus: "success", message: "User already subscribed" });
+    res
+      .status(200)
+      .send({ apiStatus: "success", message: "User already subscribed" });
   } else {
     const response = await subscribeToService(formattedSubscriberId);
     handleApiResponse(response, res);
@@ -47,13 +26,15 @@ const handleSubscribe = asyncHandler(async (req, res) => {
 
 const handleUnsubscribe = asyncHandler(async (req, res) => {
   const { subscriberId } = req.body;
-  validateSubscriberId(subscriberId, res);
-  const formattedSubscriberId = formatSubscriberId(subscriberId);
+
+  const formattedSubscriberId = validateSubscriberId(subscriberId, res);
 
   const status = await getStatus(formattedSubscriberId);
 
   if (status?.data?.subscriptionStatus === "UNREGISTERED") {
-    res.status(200).send({ apiStatus: "success", message: "User already unsubscribed" });
+    res
+      .status(200)
+      .send({ apiStatus: "success", message: "User already unsubscribed" });
   } else {
     const response = await unsubscribeFromService(formattedSubscriberId);
     handleApiResponse(response, res);
@@ -62,8 +43,8 @@ const handleUnsubscribe = asyncHandler(async (req, res) => {
 
 const getSubscriberStatus = asyncHandler(async (req, res) => {
   const { subscriberId } = req.body;
-  validateSubscriberId(subscriberId, res);
-  const formattedSubscriberId = formatSubscriberId(subscriberId);
+
+  const formattedSubscriberId = validateSubscriberId(subscriberId, res);
 
   const response = await getStatus(formattedSubscriberId);
   handleApiResponse(response, res);
